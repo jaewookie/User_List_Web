3037fec1446e1e43b137c4d846c8b340d4ee1c73
jaewookie <kjoop@naver.com>
2019-12-31 17:49:19 +0900

    get, post method, using DataBase

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ var ejs = require('ejs')
 require('dotenv').config()
 var bodyParser = require('body-parser')
 
+var listRouter = require('./router/index.js')
+
 app.set('views',path.join(__dirname,'views'))
 app.set('view engine','ejs')
 
 app.use(express.static(path.join(__dirname,'public')))
 app.use(bodyParser.urlencoded({extend : false}))
 
-
+app.use('/', listRouter)
 
 var port = process.env.PORT || 3000
 app.listen(port,()=>{
--- router/index.js
+++ router/index.js
@@ -3,10 +3,56 @@ var router = express.Router()
 var mysql = require('mysql')
 require('dotenv').config()
 
-mysql.createConnection({
+var db = mysql.createConnection({
     host:process.env.DB_HOST,
     user:process.env.DB_USER,
     password:process.env.DB_PW,
     database:process.env.DB_NAME,
+})
+
+router.get('/topic/add',(req, res)=>{
+    var sql = 'SELECT * FROM topic'
+    db.query(sql, (err, result)=>{
+        if(!err){
+        console.log(result)
+        res.render('add', {topics : result})
+        }else{
+        console.log(err)
+        }
+    })
     
 })
+
+router.post('/topic/add', (req, res)=>{
+    var title = req.body.title
+    var description = req.body.description
+    var author = req.body.author
+    
+    var sql = 'INSERT INTO topic (title, description, author) VALUES(?, ?, ?)'
+
+    var params = [title, description, author]
+    db.query(sql, params, (err, result)=>{
+        if(!err){
+            console.log("Sucess")
+            res.redirect('/topic/add')
+        } else{
+            console.log(err)
+        }
+    })
+
+})
+
+router.get('/topic/:id', (req, res)=>{
+    // console.log(req.params.id)
+    var id = req.params.id
+    var sql = 'SELECT * FROM topic WHERE id=?'
+    db.query(sql, [id], (err, result)=>{
+        if(!err){
+            res.render('test.ejs', {topic:result})
+        } else{
+            console.log(err)
+        }
+    })
+})
+
+module.exports=router
